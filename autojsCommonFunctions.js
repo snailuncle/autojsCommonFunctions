9351954423da36b3caf580d73e77442ced3788f9
snailuncle <1789500304@qq.com>
2019-02-24 07:42:03 +0800

    autojsCommonFunctions

--- autojsCommonFunctions.js
+++ autojsCommonFunctions.js
@@ -10,6 +10,18 @@
  * git:  https://github.com/snailuncle/autojsCommonFunctions/blob/master/autojsCommonFunctions.js
  */
 
+//  //导入模块
+//  function 导入常用函数模块(){
+//   var url='https://raw.githubusercontent.com/snailuncle/autojsCommonFunctions/master/autojsCommonFunctions.js'
+//   var r = http.get(url)
+//   log("code = " + r.statusCode);
+//   var html=r.body.bytes()
+//   files.writeBytes('./autojsCommonFunctions.js',html)
+//   var common=require('./autojsCommonFunctions.js')
+//   return common
+// }
+// var common=导入常用函数模块()
+
 
 
 [
