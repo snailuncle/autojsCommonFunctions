fe20bfe4171ac9ff6ba8eaf70a4fa5a5cd95f99f
snailuncle <1789500304@qq.com>
2019-03-01 20:40:45 +0800

    first commit

--- autojsCommonFunctions.js
+++ autojsCommonFunctions.js
@@ -790,7 +790,7 @@ common.安卓intent源码=function (){
 
 }
 
-common.获取手机ip地理位置=function (){
+common.获取手机ip地理位置 = function () {
   var ip地理位置 = false
   var ip地理位置正则 = /本机IP:&nbsp;\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}<\/span>([\s\S]*?)<\/td/
   var ipUrl = "http://www.baidu.com/s?ie=UTF-8&wd=ip%E5%BD%92%E5%B1%9E%E5%9C%B0%E6%9F%A5%E8%AF%A2"
@@ -800,14 +800,32 @@ common.获取手机ip地理位置=function (){
   ip地理位置 = ip地理位置正则.exec(htmlResult)
   if (ip地理位置) {
     ip地理位置 = ip地理位置正则.exec(ip地理位置)
-    ip地理位置 = ip地理位置[1]
-    toastLog(ip地理位置)
-    return ip地理位置
+    ip地理位置 = ip地理位置[0]
+    if (ip地理位置) {
+      var ip正则 = /\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}/
+      var 地理位置正则 = /span>.+?\t/
+      var ip = ip地理位置.match(ip正则)
+      var 地理位置 = ip地理位置.match(地理位置正则)
+      if (ip) {
+        ip = ip[0]
+      }
+      if (地理位置) {
+        地理位置 = 地理位置[0].replace(/(span>|\t)/g, "")
+      }
+      var info = {
+        ip: ip,
+        地理位置: 地理位置
+      }
+      toastLog(info)
+      return info
+    } else {
+      log('没有查询到Ip地理位置,脚本停止')
+      return false
+    }
   } else {
     log('没有查询到Ip地理位置,脚本停止')
     return false
   }
-
 }
 
 common.获取app图标=function (appName){
